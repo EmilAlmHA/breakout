26765abe21a3eab6513fa7aa75d4be49eb6d5174
Fredde131 <frederik.lindholm@ha.ax>
2025-03-24 10:11:58 +0200

    test folder
    for testing things

---
+++ Tests/test1.py
@@ -0,0 +1,20 @@
+import pygame
+import pygame_menu 
+
+pygame.init()
+surface = pygame.display.set_mode((600, 400))
+
+def set_difficulty(value, difficulty): 
+
+    pass
+
+def start_game():
+
+    pass
+
+menu = pygame_menu.Menu('Welcome!', 400, 300, theme=pygame_menu.themes.THEME_BLUE)
+menu.add.text_input('Name: ', default='Your mom')
+menu.add.selector('Difficulty: ', [('Hard', 1), ('Medium', 2), ('Easy', 3), ('Baby', 4)], onchange=set_difficulty)
+menu.add.button('Play', start_game)
+menu.add.button('Quit', pygame_menu.events.EXIT)
+menu.mainloop(surface)
