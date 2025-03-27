7d1e131befebbe0c3ff00b4a86f9376c8eabcea5
Fredde131 <frederik.lindholm@ha.ax>
2025-03-27 12:59:04 +0200

    put in menu, movement, boundaries, size changeing

--- Tests/test1.py
+++ Tests/test1.py
@@ -1,5 +1,5 @@
 import pygame
-import pygame_menu 
+import pygame_menu
 
 pygame.init()
 surface = pygame.display.set_mode((600, 400))
---
+++ Tests/test3_almsog.py
@@ -0,0 +1,94 @@
+import pygame
+import sys
+import pygame_menu
+
+
+pygame.init()
+surface = pygame.display.set_mode((800, 534))
+
+pygame.display.set_caption('Testing')
+clock = pygame.time.Clock()
+font = pygame.font.Font(None, 50)
+player_lenght = '---'
+hill = pygame.image.load('Tests/Success-Story-KANAGWA-1.png')
+text_surface = font.render(f'{player_lenght}', False, 'Yellow')
+text_x_pos = 400
+
+# Color definitions (ensure these are correctly defined)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+current_color = 'Yellow'
+
+
+def set_difficulty(value, difficulty): 
+    pass
+
+
+def set_color(value, color):
+    global current_color
+    if value == 1:
+        current_color = RED
+    elif value == 2:
+        current_color = GREEN
+    elif value == 3:
+        current_color = BLUE
+    elif value == 4:
+        current_color = WHITE
+    elif value == 5:
+        current_color = BLACK
+
+
+def menu():
+    menu = pygame_menu.Menu('Welcome!', 400, 300, theme=pygame_menu.themes.THEME_BLUE)
+    menu.add.button('Play', start_game)
+    menu.add.text_input('Name: ', default='Change this...')
+    menu.add.selector('Difficulty: ', [('Hard', 1), ('Medium', 2), ('Easy', 3), ('Baby', 4)], onchange=set_difficulty)
+    menu.add.selector('Color', [('Red', 1), ('Green', 2), ('Blue', 3), ('White', 4), ('Black', 5)], onchange=set_color)
+    menu.add.button('Quit', pygame_menu.events.EXIT)
+    menu.mainloop(surface)
+
+
+def start_game():
+    global player_lenght, text_surface, text_x_pos, current_color
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    text_x_pos -= 10
+                    print('Left')
+
+                elif event.key == pygame.K_RIGHT:
+                    text_x_pos += 10
+                    print('Right')
+
+                elif event.key == pygame.K_UP:
+                    player_lenght += '-'
+                    text_surface = font.render(f'{player_lenght}', False, current_color)
+                    print('Up')
+
+                elif event.key == pygame.K_DOWN:
+                    player_lenght = player_lenght[:-1]
+                    text_surface = font.render(f'{player_lenght}', False, current_color)
+                    print('Down')
+
+
+                elif event.key == pygame.K_ESCAPE:
+                    exit()
+
+
+        surface.blit(hill, (0, 0))
+        surface.blit(text_surface, (text_x_pos, 250))
+        pygame.display.update()
+        clock.tick(60)
+
+
+menu()
